c694ba85f2d2be95428011c2a46336b506ddadfb
Smuliii <smulitus@gmail.com>
2020-09-05 01:22:41 +0300

    Fix part 4 backend tests

--- part4/tests/list-api.test.js
+++ part4/tests/list-api.test.js
@@ -1,10 +1,20 @@
 const supertest = require('supertest');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const app = require('../app');
 const Blog = require('../models/blog');
-const User = require('../models/blog');
+const User = require('../models/user');
 
 const api = supertest(app);
+
+const userLogin = async user => {
+	return await api
+		.post('/api/login')
+		.send({
+			username: user.username,
+			password: user.password,
+		});
+};
 
 const initialBlogs = [{
 	title: "React patterns",
@@ -40,9 +50,21 @@ const newBlog = {
 	likes: 2,
 };
 
+const user = {
+	username: 'johndoe',
+	password: 'secret',
+	name: 'John Doe',
+}
+
 beforeEach(async () => {
+	// Add dummy data
 	await Blog.deleteMany({});
 	await Blog.insertMany(initialBlogs);
+	await User.deleteMany({});
+	await new User({
+		...user,
+		password: await bcrypt.hash(user.password, 10),
+	}).save();
 });
 
 test('should return blog list as json', async () => {
@@ -67,9 +89,11 @@ test('should have id property for every blog', async () => {
 });
 
 test('should create a new blog item succesfully', async () => {
+	const login = await userLogin(user);
 	const response = await api
 		.post('/api/blogs')
 		.send(newBlog)
+		.set('Authorization', `Bearer ${login.body.token}`)
 		.expect(201)
 		.expect('Content-Type', /application\/json/);
 
@@ -85,9 +109,11 @@ test('should default to 0 if likes are missing', async () => {
 	const newBlogWithoutLikes = { ...newBlog };
 	delete newBlogWithoutLikes.likes;
 
+	const login = await userLogin(user);
 	const response = await api
 		.post('/api/blogs')
 		.send(newBlogWithoutLikes)
+		.set('Authorization', `Bearer ${login.body.token}`)
 		.expect(201);
 
 	expect(response.body.likes).toBeDefined();
@@ -97,9 +123,11 @@ test('should give error if title is missing', async () => {
 	const newBlogWithoutLikes = { ...newBlog };
 	delete newBlogWithoutLikes.title;
 
+	const login = await userLogin(user);
 	await api
 		.post('/api/blogs')
 		.send(newBlogWithoutLikes)
+		.set('Authorization', `Bearer ${login.body.token}`)
 		.expect(400);
 });
 
@@ -107,17 +135,22 @@ test('should give error if url is missing', async () => {
 	const newBlogWithoutLikes = { ...newBlog };
 	delete newBlogWithoutLikes.url;
 
+	const login = await userLogin(user);
 	await api
 		.post('/api/blogs')
 		.send(newBlogWithoutLikes)
+		.set('Authorization', `Bearer ${login.body.token}`)
 		.expect(400);
 });
 
 test('should delete blog item succesfully', async () => {
 	const { body: blogs } = await api.get('/api/blogs');
 
+	const login = await userLogin(user);
 	await api
 		.delete(`/api/blogs/${blogs[0].id}`)
+		// TODO: supertest's delete method won't accept headers..?
+		.set('Authorization', `Bearer ${login.body.token}`)
 		.expect(204);
 
 	const { body: updatedBlogs } = await api.get('/api/blogs');
@@ -140,9 +173,22 @@ test('should update blog item succesfully', async () => {
 	expect(response.body.likes).toBe(data.likes);
 });
 
-test('should ', async () => {
+test('should give error if token is missing', async () => {
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(401);
+});
 
-})
-
+test('should return token for valid user', async () => {
+	await api
+		.post('/api/login')
+		.send({
+			username: user.username,
+			password: user.password,
+		})
+		.expect(200)
+		.expect('Content-Type', /application\/json/);
+});
 
 afterAll(() => mongoose.connection.close());
