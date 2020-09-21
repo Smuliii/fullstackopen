440a73f8edd77691deef57f2adec837eb13025ad
Smuliii <smulitus@gmail.com>
2020-09-21 12:54:39 +0300

    Add part 6 unicafe-redux

---
+++ part6/unicafe-redux/src/reducer.test.js
@@ -0,0 +1,71 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const action = {
+      type: 'DO_NOTHING'
+    }
+    const state = undefined
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = deepFreeze(initialState)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = deepFreeze(initialState)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = deepFreeze(initialState)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    })
+  })
+
+  test('should reset all counters', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = deepFreeze({
+      good: 1,
+      ok: 2,
+      bad: 3,
+    })
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({ ...initialState })
+  })
+})
